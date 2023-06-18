8d7a7e690abe10d7a76b6919a344ff6873327efc
Samuel Li <shaomenglee@gmail.com>
2023-06-17 22:54:31 -0600

    check in before adding slice_norm support

--- utilities/muram_sperr.cpp
+++ utilities/muram_sperr.cpp
@@ -8,8 +8,8 @@
 
 int main(int argc, char* argv[])
 {
-  if (argc != 6) {
-    std::cout << "Usage: ./muram_sperr  infile  dimx  dimy  dimz  outfile" << std::endl;
+  if (argc < 6) {
+    std::cout << "Usage: ./muram_sperr  infile  dimx  dimy  dimz  outfile [psnr]" << std::endl;
     return 0;
   }
 
@@ -19,11 +19,15 @@ int main(int argc, char* argv[])
   const size_t dimy = std::stoi(argv[3]);
   const size_t dimz = std::stoi(argv[4]);
   const auto total_len = dimx * dimy * dimz;
+  auto psnr = 130.0;
+  if (argc = 7)
+    psnr = std::stod(argv[6]);
 
   // Read input file
   auto inbuf = sperr::read_whole_file<float>(infile);
   if (inbuf.size() != total_len) {
     std::cout << "Read input file wrong!" << std::endl;
+    std::printf("  Expecting %lu floats, got %lu floats\n", total_len, inbuf.size());
     return __LINE__;
   }
 
@@ -38,12 +42,17 @@ int main(int argc, char* argv[])
     return __LINE__;
   }
   meta_size = mkit::retrieve_log_meta_len(meta);
+  auto b8 = mkit::unpack_8_booleans(reinterpret_cast<uint8_t*>(meta)[8]);
+  if (b8[0])
+    std::cout << "(Used a neg value mask)" << std::endl;
+  if (b8[1])
+    std::cout << "(Used a zero value mask)" << std::endl;
 
   // Apply SPERR compression
   auto encoder = std::make_unique<sperr::SPERR3D_OMP_C>();
   encoder->set_dims_and_chunks({dimx, dimy, dimz}, {288, 256, 256});
-  encoder->set_num_threads(6);
-  encoder->set_psnr(130.0);
+  encoder->set_num_threads(7);
+  encoder->set_psnr(psnr);
   auto rtn = encoder->compress(inbuf.data(), inbuf.size());
   if (rtn != sperr::RTNType::Good) {
     std::cout << "SPERR compression failed!" << std::endl;
@@ -52,10 +61,12 @@ int main(int argc, char* argv[])
   }
   auto stream = encoder->get_encoded_bitstream();
   encoder.reset();
+  auto comp_size = stream.size() + meta_size;
+  std::cout << "Compression bitrate: " << 8.0 * comp_size / total_len << std::endl;
 
   // Apply SPERR decompression
   auto decoder = std::make_unique<sperr::SPERR3D_OMP_D>();
-  decoder->set_num_threads(6);
+  decoder->set_num_threads(7);
   decoder->setup_decomp(stream.data(), stream.size());
   rtn = decoder->decompress(stream.data());
   if (rtn != sperr::RTNType::Good) {
